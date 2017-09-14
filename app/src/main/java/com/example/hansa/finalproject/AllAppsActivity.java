414b4f5c02c63e4e95df59a05e8e583162bef78e
Dharmang Patel <dharmangp25@gmail.com>
2017-09-13 20:45:03 -0400

    Few changes in Layout and Class name

--- app/src/main/java/com/example/hansa/finalproject/AllAppsActivity.java
+++ app/src/main/java/com/example/hansa/finalproject/AllAppsActivity.java
@@ -3,7 +3,7 @@ package com.example.hansa.finalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AllAppsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
